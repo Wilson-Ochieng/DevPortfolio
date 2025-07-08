b6c355ecf588ab822acf8f317da70fcc72e83547
Wilson-Ochieng <wilsonochieng718@gmail.com>
2025-07-08 12:09:41 +0300

    Completed my Portfolio

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings: ["Software Engineer", "Mobile Engineer", "Software Assurance Testing Expert"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
